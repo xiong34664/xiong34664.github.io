3169a3e65f41fdef3aada5040b65c0ab7d0b1313
xiong34664 <1658082169@qq.com>
2018-02-06 17:28:48 +0800

    Site updated: 2018-02-06 17:28:44

--- photos/ins.js
+++ photos/ins.js
@@ -117,16 +117,16 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'http://p1ju2a9a7.bkt.clouddn.com/min_photos/' + data.link[i] + '?imageMogr2/auto-orient';
+            var minSrc = 'http://p3puarvng.bkt.clouddn.com/image/max/' + data.link[i] + '.jpg?imageView2/1/w/170/h/170/interlace/0/q/100';
             //var src = 'https://raw.githubusercontent.com/fakeYanss/Blog_Album/master/photos/' + data.link[i];
-            var src = 'http://p1ju2a9a7.bkt.clouddn.com/' + data.link[i];
+            var src = 'http://p3puarvng.bkt.clouddn.com/image/max/' + data.link[i]+'.jpg';
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
-            src += '?imageMogr2/auto-orient';
+            src += '?imageView2/1/w/1080/h/1080/interlace/1/q/75|watermark/2/text/5b2S/font/5b6u6L2v6ZuF6buR/fontsize/400/fill/I0Y4RTNFMw==/dissolve/48/gravity/SouthEast/dx/10/dy/10|imageslim';
   
             liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                    <img class="reward-img" data-type="' + type + '" data-src="' + src + '" src="./assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                    <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="./assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
               </figure>';
